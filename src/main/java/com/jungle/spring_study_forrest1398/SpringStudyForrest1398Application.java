28e6e1ee74ba5d45198c3cda0a6351c6d185c82b
forrest1398 <129135885+forrest1398@users.noreply.github.com>
2024-06-05 23:31:07 +0900

    first commit

---
+++ src/main/java/com/jungle/spring_study_forrest1398/SpringStudyForrest1398Application.java
@@ -0,0 +1,13 @@
+package com.jungle.spring_study_forrest1398;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyForrest1398Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudyForrest1398Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jungle/spring_study_forrest1398/SpringStudyForrest1398ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungle.spring_study_forrest1398;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyForrest1398ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
